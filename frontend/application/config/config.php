af40924d159b30d0461294bfdad9647a7c277d17
itsifdal <100899601+itsifdal@users.noreply.github.com>
2022-07-19 15:54:36 +0800

    Update config.php

--- frontend/application/config/config.php
+++ frontend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://itsifdal.github.io/';
+$config['base_url'] = 'https://itsifdal.github.io/frontend';
 
 /*
 |--------------------------------------------------------------------------
